a5164b7264db98c76281a4c3729d5939e1067e6a
David Culemann <davidculemann@gmail.com>
2022-01-26 22:56:54 +0400

    game category selectable from the main menu and changes the game mode in GameScreen

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -2,43 +2,49 @@ import {
   Button,
   FormControl,
   FormControlLabel,
-  FormLabel,
   Radio,
   RadioGroup,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
-export function HomeScreen(): JSX.Element {
-  const [category, setCategory] = useState<string>("area");
+interface HomeScreenProps {
+  category: string;
+  setCategory: (input: string) => void;
+}
 
+export function HomeScreen(props: HomeScreenProps): JSX.Element {
   return (
     <div className="home-screen">
-      <Link to="/play">
-        <Button variant="contained" onClick={() => console.log(category)}>
-          Play
-        </Button>
-      </Link>
       <div className="game-options">
+        <h2>Game category:</h2>
         <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label">
+          {/* <FormLabel id="demo-radio-buttons-group-label">
             Game category
-          </FormLabel>
+          </FormLabel> */}
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            value={props.category}
+            onChange={(e) => props.setCategory(e.target.value)}
             name="radio-buttons-group"
           >
-            <FormControlLabel value="area" control={<Radio />} label="area" />
+            <FormControlLabel value="area" control={<Radio />} label="Area" />
             <FormControlLabel
               value="population"
               control={<Radio />}
-              label="population"
+              label="Population"
             />
           </RadioGroup>
         </FormControl>
       </div>
+      <Link to="/play">
+        <Button
+          variant="contained"
+          style={{ fontSize: "25px" }}
+          onClick={() => console.log(props.category)}
+        >
+          Play
+        </Button>
+      </Link>
     </div>
   );
 }
