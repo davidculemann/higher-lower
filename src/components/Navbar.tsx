64890528f10b3ef20acbefb2cb5cb5de34264ec7
David Culemann <davidculemann@gmail.com>
2022-01-27 13:01:08 +0400

    user login feature added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,50 @@
 import { Button, InputAdornment, TextField } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
+import Stack from "@mui/material/Stack";
+import axios from "axios";
+import { config } from "dotenv";
 import { useState } from "react";
+import { UserType } from "../utils/types/userType";
 // import { RiArrowUpDownLine } from "react-icons/ri";
 
-export function Navbar(): JSX.Element {
-  const [user, setUser] = useState<string>("");
+config();
+const serverBaseURL = process.env.REACT_APP_API_BASE;
 
-  const handleLogin = () => {
-    console.log(`${user} just tried to login`);
+interface NavBarProps {
+  loggedInUser: string;
+  setLoggedInUser: (input: string) => void;
+}
+
+export function Navbar(props: NavBarProps): JSX.Element {
+  const [usernameInput, setUsernameInput] = useState<string>("");
+
+  const handleSignIn = async () => {
+    try {
+      console.log(
+        `${usernameInput} just tried to login or signup, checking if new user...`
+      );
+      const usersResponse = await axios.get(`${serverBaseURL}users`);
+      if (
+        usersResponse.data.map((e: UserType) => e.name).includes(usernameInput)
+      ) {
+        //login the user, since they exist (not ideal method of authorisation but good enough for now)
+        props.setLoggedInUser(usernameInput);
+        console.log(`${usernameInput} was logged in`);
+        setUsernameInput("");
+      } else {
+        //signup the user, then log them in
+        await axios.post(`${serverBaseURL}users/${usernameInput}`);
+        console.log(`${usernameInput} was signed up and logged in`);
+        props.setLoggedInUser(usernameInput);
+        setUsernameInput("");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSignOut = () => {
+    props.setLoggedInUser("");
   };
 
   return (
@@ -18,22 +55,42 @@ export function Navbar(): JSX.Element {
         <span>&nbsp;</span>
         {/* <RiArrowUpDownLine /> */}
       </h1>
-      <div className="login-div">
-        <TextField
-          id="standard-name"
-          label="Username"
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
-          InputProps={{
-            endAdornment: <Button onClick={() => handleLogin()}>Login</Button>,
-            startAdornment: (
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </div>
+      {props.loggedInUser === "" ? (
+        <div className="login-div">
+          <TextField
+            id="standard-name"
+            label="Username"
+            value={usernameInput}
+            onChange={(e) => setUsernameInput(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <Button onClick={() => handleSignIn()}>Login</Button>
+              ),
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </div>
+      ) : (
+        <div className="logged-in-div">
+          <Stack
+            display="flex"
+            spacing={2}
+            direction="row"
+            m={3}
+            alignItems="center"
+          >
+            <AccountCircle className="logged-in-icon" />
+            <h2>{props.loggedInUser}</h2>
+            <Button variant="outlined" onClick={handleSignOut}>
+              Sign out
+            </Button>
+          </Stack>
+        </div>
+      )}
     </div>
   );
 }
